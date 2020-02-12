ca58a39bf41fad19c3e615369b7ca2c6e9a825f0
Adewale Orotayo <didymus7007@gmail.com>
2020-02-12 21:47:50 +0100

    All print and put statement form game-logic test moved to main test

--- main-test.rb
+++ main-test.rb
@@ -52,17 +52,22 @@ while !/^y(es){0,1}$/i.match(answer) || !/^no{0,1}$/i.match(answer)
 
     board = Board.new
 
-    board.availible_spaces.each do |x|
-      separator = [1, 2, 4, 5, 7, 8]
-      print x < 10 ? "  #{board.availible_spaces[x - 1]} " : " #{board.availible_spaces[x - 1]}"
-      print '|' if separator.include?(x)
-      print "\n" if (x % 3).zero?
+    def print_board(game_board)
+      game_board.each_with_index do |x, y|
+        separator = [1, 2, 4, 5, 7, 8]
+        print "  #{game_board[y]} "
+        print '|' if separator.include?(y + 1)
+        print "\n" if ((y + 1).to_i % 3).zero?
+      end
     end
+
+    print_board(board.availible_spaces)
+    
     until game.result
       puts "\n#{game.active_player.name} pick a number to put your game piece"
-      pick = gets.chomp
-      print pick, game.active_player.mark, game.active_player.choices
+      pick = gets.chomp.to_i
       board.space_selection(pick, game.active_player.mark, game.active_player.choices)
+      print_board(board.availible_spaces)
       if board.win_validation(game.active_player.choices)
         puts "Congratulations #{game.active_player.name}, you're a winner"
         break
