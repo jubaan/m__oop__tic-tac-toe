65e2769c359fe9f409a5718d440c78282288db42
Adewale Orotayo <didymus7007@gmail.com>
2020-02-12 22:20:50 +0100

    Interface file created and required in main

--- bin/main.rb
+++ bin/main.rb
@@ -1,34 +1,6 @@
+require_relative '../interface'
 require_relative '../lib/game_logic'
 
-puts "\n" + '*' * 50
-puts "\n          ---- Tic Tac TRONIX 2000 ----"
-puts "\n        An strategy game for two players."
-puts "\n             Creative Commons 2020"
-puts "\n" + '*' * 50
-puts "\n Tic Tac TRONIX 2000 is based on the classic Tic
- Tac Toe game."
-
-puts "\n INSTRUCTIONS: to play select \"Y\", you'll be
- asked to introduce the name of both Players. Once
- set up, players will take turns to leave their
- marks (X's or O's) on the board. The player who
- first has 3 marks in a row wins the game."
-
-puts "\n                   __RULES__
-\n1. The game is played on a grid that's 3x3 squares.
-2. Player One is X's and Player Two is 0's.
-3. X always starts the game.
-4. Players take turns putting their marks in unselected
-   squares.
-5. The first player to get 3 marks in a row (up,
-   down, across, or diagonally) is the winner.
-6. When all 9 squares are full, the game is over.
-   If no player has 3 marks in a row, the game ends
-   in a tie."
-
-prompt = '> '
-
-print "\nDo you wanna play? Y/n: "
 answer = gets.chomp
 
 while !/^y(es){0,1}$/i.match(answer) || !/^no{0,1}$/i.match(answer)
