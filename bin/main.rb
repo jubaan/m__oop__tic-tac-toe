f5cdec4ece3c0f9e9f607a264d0121a642a15293
Adewale Orotayo <didymus7007@gmail.com>
2020-02-13 00:05:22 +0100

    Linter fix: block level nesting_11

--- bin/main.rb
+++ bin/main.rb
@@ -6,65 +6,70 @@ answer = gets.chomp
 
 while !/^y(es){0,1}$/i.match(answer) || !/^no{0,1}$/i.match(answer)
    if /^y(es){0,1}$/i.match(answer)
-    prompt = '> '
-    puts "\nPlayer 1 choose your name: "
-    print prompt
-    player1 = Player.new(gets.chomp.upcase)
+     prompt = '> '
+     puts "\nPlayer 1 choose your name: "
+     print prompt
+     player1 = Player.new(gets.chomp.upcase)
+ 
+     puts "\nNow, Player 2 choose your name: "
+     print prompt
+     player2 = Player.new(gets.chomp.upcase)
+ 
+     game = Game.new(player1, player2, answer)
 
-    puts "\nNow, Player 2 choose your name: "
-    print prompt
-    player2 = Player.new(gets.chomp.upcase)
+     puts "\nWelcome players!"
+     puts "\n#{player1.name} you'll play as the '#{player1.mark}' and
+     #{player2.name} you'll play as the '#{player2.mark}'."
 
-    game = Game.new(player1, player2, answer)
+     puts "\nThis is the board\n"
 
-    puts "\nWelcome players!"
-    puts "\n#{player1.name} you'll play as the '#{player1.mark}' and
-    #{player2.name} you'll play as the '#{player2.mark}'."
+     board = Board.new
 
-    puts "\nThis is the board\n"
 
-    board = Board.new
 
-    def print_board(game_board)
-      game_board.each_with_index do |_x, y|
-        separator = [1, 2, 4, 5, 7, 8]
-        print "  #{game_board[y]} "
-        print '|' if separator.include?(y + 1)
-        print "\n" if ((y + 1).to_i % 3).zero?
-      end
-    end
-    print_board(board.availible_spaces)
 
-    def check(game_board, console)
-      if game_board.win_validation(console.active_player.choices)
-        puts "Congratulations #{console.active_player.name}, you're a winner"
-      elsif game_board.draw_validation
-        puts "It's a draw"
-      end
-    end
-    until game.result
-      puts "\n#{game.active_player.name} pick a number to put your game piece"
-      pick = gets.chomp.to_i
-      board.space_selection(pick, game.active_player.mark, game.active_player.choices)
-      print_board(board.availible_spaces)
-      check(board, game)
-      break if board.win_validation(game.active_player.choices) || board.draw_validation
 
-      game.switch_players
-    end
 
-    break
+     def print_board(game_board)
+        game_board.each_with_index do |_x, y|
+          separator = [1, 2, 4, 5, 7, 8]
+          print "  #{game_board[y]} "
+          print '|' if separator.include?(y + 1)
+          print "\n" if ((y + 1).to_i % 3).zero?
+        end
+     end
+     print_board(board.availible_spaces)
 
-  elsif /^no{0,1}$/i.match(answer)
+     def check(game_board, console)
+        if game_board.win_validation(console.active_player.choices)
+           puts "Congratulations #{console.active_player.name}, you're a winner"
+        elsif game_board.draw_validation
+           puts "It's a draw"
+        end
+     end
+     until game.result
+     puts "\n#{game.active_player.name} pick a number to put your game piece"
+     pick = gets.chomp.to_i
+     board.space_selection(pick, game.active_player.mark, game.active_player.choices)
+     print_board(board.availible_spaces)
+     check(board, game)
+     break if board.win_validation(game.active_player.choices) || board.draw_validation
 
-    puts 'Goodbye...'
-    break
+     game.switch_players
+     end
 
-  else
+     break
 
-    puts "\nDidn't get you. You said you wanna play: Yes or No?"
-    print prompt
-    answer = gets.chomp
+   elsif /^no{0,1}$/i.match(answer)
 
-  end
+     puts 'Goodbye...'
+     break
+
+   else
+
+     puts "\nDidn't get you. You said you wanna play: Yes or No?"
+     print prompt
+     answer = gets.chomp
+
+   end
 end
