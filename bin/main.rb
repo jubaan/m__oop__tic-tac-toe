f398b3226f7fe56201165c5081583e6cc3e9e98a
Adewale Orotayo <didymus7007@gmail.com>
2020-02-13 00:21:31 +0100

    Linter fix: block level nesting_12

--- bin/main.rb
+++ bin/main.rb
@@ -5,7 +5,7 @@ prompt = '>'
 answer = gets.chomp
 
 while !/^y(es){0,1}$/i.match(answer) || !/^no{0,1}$/i.match(answer)
-   if /^y(es){0,1}$/i.match(answer)
+  if /^y(es){0,1}$/i.match(answer)
      prompt = '> '
      puts "\nPlayer 1 choose your name: "
      print prompt
@@ -16,60 +16,52 @@ while !/^y(es){0,1}$/i.match(answer) || !/^no{0,1}$/i.match(answer)
      player2 = Player.new(gets.chomp.upcase)
  
      game = Game.new(player1, player2, answer)
-
+ 
      puts "\nWelcome players!"
      puts "\n#{player1.name} you'll play as the '#{player1.mark}' and
      #{player2.name} you'll play as the '#{player2.mark}'."
-
+ 
      puts "\nThis is the board\n"
-
+ 
      board = Board.new
-
-
-
-
-
-
+ 
      def print_board(game_board)
-        game_board.each_with_index do |_x, y|
-          separator = [1, 2, 4, 5, 7, 8]
-          print "  #{game_board[y]} "
-          print '|' if separator.include?(y + 1)
-          print "\n" if ((y + 1).to_i % 3).zero?
-        end
+       game_board.each_with_index do |_x, y|
+         separator = [1, 2, 4, 5, 7, 8]
+         print "  #{game_board[y]} "
+         print '|' if separator.include?(y + 1)
+         print "\n" if ((y + 1).to_i % 3).zero?
+       end
      end
      print_board(board.availible_spaces)
-
-     def check(game_board, console)
-        if game_board.win_validation(console.active_player.choices)
-           puts "Congratulations #{console.active_player.name}, you're a winner"
-        elsif game_board.draw_validation
-           puts "It's a draw"
-        end
-     end
+     
      until game.result
-     puts "\n#{game.active_player.name} pick a number to put your game piece"
-     pick = gets.chomp.to_i
-     board.space_selection(pick, game.active_player.mark, game.active_player.choices)
-     print_board(board.availible_spaces)
-     check(board, game)
-     break if board.win_validation(game.active_player.choices) || board.draw_validation
-
-     game.switch_players
+       puts "\n#{game.active_player.name} pick a number to put your game piece"
+       pick = gets.chomp.to_i
+       board.space_selection(pick, game.active_player.mark, game.active_player.choices)
+       print_board(board.availible_spaces)
+       if board.win_validation(game.active_player.choices)
+         puts "Congratulations #{game.active_player.name}, you're a winner"
+         break
+       elsif board.draw_validation
+         puts "It's a draw"
+         break
+       end
+       game.switch_players
      end
-
+ 
      break
 
-   elsif /^no{0,1}$/i.match(answer)
+  elsif /^no{0,1}$/i.match(answer)
 
-     puts 'Goodbye...'
-     break
+    puts 'Goodbye...'
+    break
 
-   else
+  else
 
-     puts "\nDidn't get you. You said you wanna play: Yes or No?"
-     print prompt
-     answer = gets.chomp
+    puts "\nDidn't get you. You said you wanna play: Yes or No?"
+    print prompt
+    answer = gets.chomp
 
-   end
+  end
 end
