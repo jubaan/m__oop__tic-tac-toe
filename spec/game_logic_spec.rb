4c7be09cb6835749be5fde4c0ce08c22b26bdb1d
jubaan <julio.ab@outlook.com>
2020-02-19 12:48:28 -0600

    Fix: linter error

--- spec/game_logic_spec.rb
+++ spec/game_logic_spec.rb
@@ -2,13 +2,12 @@ require './lib/game_logic'
 
 RSpec.describe 'testing game logic' do
   context 'Player' do
-    it 'retrieve player1 name and mark' do
+    it 'returns player1 mark' do
       player1 = Player.new('player1')
-      expect(player1.name).to eql('player1')
       expect(player1.mark).to eql('+')
     end
 
-    it 'retrieve player2 name and mark' do
+    it 'returns player2 mark' do
       player2 = Player.new('player2')
       expect(player2.mark).to eql('-')
     end
