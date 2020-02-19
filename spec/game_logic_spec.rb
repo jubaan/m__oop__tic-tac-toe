8b7038171f5da74db5ed4bf9e3f14e59bc8febd8
Adewale Orotayo <didymus7007@gmail.com>
2020-02-19 19:23:14 +0100

    Board Test: space_selection; win_validation and draw_validation

--- spec/game_logic_spec.rb
+++ spec/game_logic_spec.rb
@@ -1,4 +1,10 @@
-require '../lib/game_logic'
+require './lib/game_logic'
+
+class Board
+  def initialize(availible_spaces)
+    @availible_spaces = availible_spaces
+  end
+end
 
 RSpec.describe 'testing game logic' do
 
@@ -17,6 +23,41 @@ RSpec.describe 'testing game logic' do
   end
 
   context 'Board' do
+    describe '#space_selection' do
+      it 'returns a new board with a changed position' do
+        board = Board.new
+
+        expect(board.space_selection(4, '+', [])).to eq([4])
+      end
+    end
+
+    describe '#win_validation' do
+      it 'returns true if array matches combination' do
+        board = Board.new
+
+        expect(board.win_validation([4, 7, 1, 8, 9])).to eq(true)
+      end
+
+      it 'returns false if array does not match combination' do
+        board = Board.new
+
+        expect(board.win_validation([3, 4, 1, 8, 9])).to eq(false)
+      end
+    end
+
+    describe '#draw_validation' do
+      it 'returns false if all array elements are not strings' do
+        board = Board.new
+
+        expect(board.draw_validation).to eq(false)
+      end
+
+      it 'returns true if all array elements are strings' do
+        board = Board.new(["+", "-", "-", "+", "+", "-", "+", "+", "+",])
+
+        expect(board.draw_validation).to eq(true)
+      end
+    end
 
   end
 
